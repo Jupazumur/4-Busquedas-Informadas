f999e4054dde958b3e32988db8669a3ceaba6fb4
Jupazumur <a223201410@unison.mx>
2026-02-26 12:59:12 -0700

    Completar PbCamionMagico

--- problemas.py
+++ problemas.py
@@ -9,6 +9,7 @@ Tarea sobre búsquedas, donde lo que es importante es crear nuevas heurísticas
 """
 
 import busquedas
+from math import log2
 
 # ------------------------------------------------------------
 #  Desarrolla el modelo del Camión mágico
@@ -69,47 +70,76 @@ class PbCamionMagico(busquedas.ProblemaBusqueda):
 # ------------------------------------------------------------
 #  Desarrolla una política admisible.
 # ------------------------------------------------------------
-
-def h_1_camion_magico(nodo, N):
+def h_1_camion_magico_helper(N):
     """
-    Mínimo de pasos
-
-    Bajamos el costo del camión a 1. Ahora ambos cuestan lo mismo,
-    entonces lo que importa son los pasos. Como la heurística
-    calcula el mínimo de pasos y el costo de una acción en el real
-    es mínimo 1, la heurística siempre regresará un costo (pasos) menor.
-
+    Clausura para mantener la estructura de la comparación
     """
-    pasos = 0
-    x = nodo.estado[0]
+    def h_1_camion_magico(nodo):
+        """
+        Mínimo de pasos
 
-    while N > x:
-        if N % 2 == 0 and N/2 >= x:
-            pasos += 1
-            N //= 2
-        elif N % 2 == 0 and N-1 >= x:
-            pasos += 1
-            N -= 1
-        elif N % 2 != 0 and N-1 >= x:
-            pasos += 1
-            N -= 1
+        Bajamos el costo del camión a 1. Ahora ambos cuestan lo mismo,
+        entonces lo que importa son los pasos. Como la heurística
+        calcula el mínimo de pasos y el costo de una acción en el real
+        es mínimo 1, la heurística siempre regresará un costo (pasos) menor.
 
-    return pasos
+        """
+        pasos = 0
+        x = nodo.estado[0]
+        meta = N
+
+        while meta > x:
+            if meta % 2 == 0 and meta/2 >= x:
+                pasos += 1
+                meta //= 2
+            elif meta % 2 == 0 and meta-1 >= x:
+                pasos += 1
+                meta -= 1
+            elif meta % 2 != 0 and meta-1 >= x:
+                pasos += 1
+                meta -= 1
+
+        return pasos
+    
+    return h_1_camion_magico
                 
 # ------------------------------------------------------------
 #  Desarrolla otra política admisible.
 #  Analiza y di porque piensas que es (o no es) dominante una
 #  respecto otra política
 # ------------------------------------------------------------
-
-def h_2_camion_magico(nodo):
-
+def h_2_camion_magico_helper(N):
     """
-    DOCUMENTA LA HEURÍSTICA DE DESARROLLES Y DA UNA JUSTIFICACIÓN
-    PLATICADA DE PORQUÉ CREES QUE LA HEURÍSTICA ES ADMISIBLE
-
+    Clausura para mantener la estructura de la comparación
     """
-    return 0
+    def h_2_camion_magico(nodo):
+
+        """
+        Pasos exactos
+
+        Aquí se relaja aún más, ahora no solo todo cuesta uno,
+        ahora podemos dar pasos exactos en camión para llegar a N.
+        Multiplicamos x por 2 k veces para llegar a N.
+        x * 2^k = N
+        Entonces lo que nos interesa son los pasos (k),
+        despejamos:
+        log_2(2^k) = log_2(N / x) ---> k = log_2(N / x)
+        donde k es la cantidad exacta de veces a multiplicar
+        x * 2 para llegar a N
+
+        """
+        x = nodo.estado[0]
+
+        return log2(N/x)
+    
+    return h_2_camion_magico
+
+# Conclusión
+# La primer heurística es dominante respecto a la segunda porque la segunda es demasiado optimista.
+# La primera respeta las reglas del espacio del juego: toma en cuenta que se puede caminar e ir en
+# camión. Esto hace que se aproxime más al costo real. La segunda solo toma en cuenta el camión y
+# los pasos se vuelven muy pequeños debido al logaritmo. También se puede observar en los resultados,
+# donde a* + h1 explora muchos menos nodos que a* + h2.
 
 # ------------------------------------------------------------
 #  Desarrolla el modelo del cubo de Rubik
@@ -178,8 +208,8 @@ def compara_metodos(problema, pos_inicial, heuristica_1, heuristica_2):
     @param heuristica_2: Una función de heurística
 
     """
-    solucion1 = busquedas.busqueda_A_estrella(problema, heuristica_1, pos_inicial)
-    solucion2 = busquedas.busqueda_A_estrella(problema, heuristica_2, pos_inicial)
+    solucion1 = busquedas.busqueda_A_estrella(problema, pos_inicial, heuristica_1, )
+    solucion2 = busquedas.busqueda_A_estrella(problema, pos_inicial, heuristica_2, )
     
     print('-' * 50)
     print('Método'.center(12) + 'Costo'.center(18) + 'Nodos visitados'.center(20))
@@ -192,13 +222,13 @@ def compara_metodos(problema, pos_inicial, heuristica_1, heuristica_2):
           + str(solucion2.nodos_visitados))
     print('-' * 50 + '\n')
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
 
     # Compara los métodos de búsqueda para el problema del camión mágico
     # con las heurísticas que desarrollaste
-    #pos_inicial = XXXXXXXXXX  # <--- PONLE LA POSICIÓN INICIAL QUE QUIERAS
-    #problema = PbCamionMagico( XXXXXXXXXX )  # <--- PONLE LOS PARÁMETROS QUE NECESITES
-    #compara_metodos(problema, pos_inicial, h_1_camion_magico, h_2_camion_magico)
+    pos_inicial = (1,)
+    problema = PbCamionMagico(1000)
+    compara_metodos(problema, pos_inicial, h_1_camion_magico_helper(problema.meta), h_2_camion_magico_helper(problema.meta))
     
     # Compara los métodos de búsqueda para el problema del cubo de rubik
     # con las heurísticas que desarrollaste
